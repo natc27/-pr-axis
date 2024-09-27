a248969b684fc6382d0445356e5ae927f6545ffb
Felipe Chaves <95656388+FelipeCh18@users.noreply.github.com>
2024-09-27 13:14:29 -0500

    Aquí se implementa la descarga de csv
    se incluye tambien el timestamp de los datos para que sea más fácil importarlos al Edge AI

--- public/connect.js
+++ public/connect.js
@@ -8,6 +8,8 @@ function connectAxis(options = {}) {
       console.log(d);
     });
   const isSecure = false;
+  const vector = [];
+  timestep = 0;
 
   /** extract server url */
   const url =
@@ -22,7 +24,6 @@ function connectAxis(options = {}) {
 
   ws.onopen = function () {
     console.log(`opening websocket to ${url}`);
-
     /** we are expecting arraybuffers, data wrapped into bytes */
     ws.binaryType = 'arraybuffer';
     /**
@@ -44,8 +45,11 @@ function connectAxis(options = {}) {
     const { address, args } = packet;
     if (address === 'pn') {
       args.forEach((el) => {
+        args[0].data.RightHand.unshift(timestep);
+        vector.push(args[0].data.RightHand);
         target(el);
       });
+      timestep = timestep + 0.5;
     }
     if (address === 'settings') {
       console.log(args);
@@ -55,4 +59,66 @@ function connectAxis(options = {}) {
       /** else we received maybe a system message */
     }
   };
+  
+  ws.onclose = function(ev){
+    console.log(vector);
+    arrayObjToCsv(vector);
+  }
 }
+
+
+
+function arrayObjToCsv(ar) {
+	//comprobamos compatibilidad
+	if(window.Blob && (window.URL || window.webkitURL)){
+		var contenido = "",
+			d = new Date(),
+			blob,
+			reader,
+			save,
+			clicEvent;
+		//creamos contenido del archivo
+		for (var i = 0; i < ar.length; i++) {
+			//construimos cabecera del csv
+			if (i == 0)
+				contenido += ["timestamp", "y", "x", "z"].join(",") + "\n";
+			//resto del contenido
+			contenido += Object.keys(ar[i]).map(function(key){
+							return ar[i][key];
+						}).join(",") + "\n";
+		}
+		//creamos el blob
+		blob =  new Blob(["\ufeff", contenido], {type: 'text/csv'});
+		//creamos el reader
+		var reader = new FileReader();
+		reader.onload = function (event) {
+			//escuchamos su evento load y creamos un enlace en dom
+			save = document.createElement('a');
+			save.href = event.target.result;
+			save.target = '_blank';
+			//aquí le damos nombre al archivo
+			save.download = "datos.csv";
+			try {
+				//creamos un evento click
+				clicEvent = new MouseEvent('click', {
+					'view': window,
+					'bubbles': true,
+					'cancelable': true
+				});
+			} catch (e) {
+				//si llega aquí es que probablemente implemente la forma antigua de crear un enlace
+				clicEvent = document.createEvent("MouseEvent");
+				clicEvent.initEvent('click', true, true);
+			}
+			//disparamos el evento
+			save.dispatchEvent(clicEvent);
+			//liberamos el objeto window.URL
+			(window.URL || window.webkitURL).revokeObjectURL(save.href);
+		}
+		//leemos como url
+		reader.readAsDataURL(blob);
+	}else {
+		//el navegador no admite esta opción
+		alert("Su navegador no permite esta acción");
+	}
+};
